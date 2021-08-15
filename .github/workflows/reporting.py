7cc0c6880ebc68c7147baa2647525dc37a0c87e7
Dave McKinstry <dmckinstry@users.noreply.github.com>
2021-08-15 21:11:29 +0000

    Add checkbox counting

--- .github/workflows/reporting.py
+++ .github/workflows/reporting.py
@@ -12,6 +12,7 @@ REPOSITORY    = "ft-staging"
 #endregion
 
 #region Functions
+#
 # GetProjectData - Returns query results for FastTrack Issues + Memex
 def GetProjectData(org, project, repo):
   issue_query = """
@@ -74,50 +75,35 @@ def GetProjectData(org, project, repo):
   else:
     return
 
+#
 # MergeIssueData - Merges thhe project and issue data in a unified issue list:
-# Returns Issues list with:
-# - Id
-# - Title
-# - Labels (string with comma delimited list)
-# - Body
-# - Author
-# - LastUpdated
-# - Assignees (string with comma delimited list)
-# - Fields (JSON)
 def MergeIssueData(project_issue_results):
   issue_list = []
 
   # First, capture all issues from the query
   for node in project_issue_results['data']['organization']['repository']['issues']['nodes']:
-      issue = {}
-      issue['title']      = node['title']
-      issue['labels']     = str(list(map(lambda x:x['name'], node['labels']['nodes'])))
-      issue['author']     = node['author']['login']
-      issue['body']       = node.get('body')
-      issue['assignees']  = node.get('missing', 'none')
-      issue_list.append(issue)
+    issue = {}
+    issue['Title']      = node['title']
+    issue['Labels']     = str(list(map(lambda x:x['name'], node['labels']['nodes'])))
+    issue['Author']     = node['author']['login']
+    issue['Body']       = node.get('body')
+    issue['Assignees']  = node.get('missing', 'none')
+    issue_list.append(issue)
 
   # Next, Copy in all fields from the projectNext data
   for node in project_issue_results['data']['organization']['projectNext']['items']['nodes']:
     # There has to be a better way in python to find an item in a list
     for issue in issue_list:
       # Joining on Title as I don't have a better choice
-      if issue['title'] == node['title']:
-        issue['lastUpdated'] = node['updatedAt']
-        issue['fields'] = []
+      if issue['Title'] == node['title']:
+        # Promote custom fields
         for field in node['fieldValues']['nodes']:
-          issue['fields'].append( {field['projectField']['name']: field['value']})
+          fieldName = field['projectField']['name']
+          issue[fieldName] = field['value']
 
-    # Clean up and promote special fields (e.g., Status)
-    PromoteAndMapFields(issue_list)
   return issue_list
 
-# PromoteAndMapFields - Cleans up the 'status' field and maybe more to come...
-def PromoteAndMapFields(issues):
-  for issue in issues:
-    print('TODO: PromoteAndMap {0}', issue)
-    # Including mapping Status field from Id
-
+#
 # DatesFromIssueTitle: Parses start and end dates
 def DatesFromIssueTitle(title):
     date_range_strings = title.rsplit("(")[1].split(")")[0].split("-")
@@ -132,37 +118,70 @@ def DatesFromIssueTitle(title):
             return "Invalid date string"
     return {"start": date_range[0], "finish": date_range[1]}
 
-def FormatUrl(repo, number):
-    return 'https://github.com/github/{0}/issues/{1}'.format(repo, number)
+#
+# FormatUrl - Returns markdown for a link with the title for a given issue
+def FormatUrl(org, repo, issue_id):
+    return 'https://github.com/{0}/{1}/issues/{2}'.format(org, repo, issue_id)
 
 #
-# Project board details for recent and upcoming deliveries
-#
+# ColunmnDetails - Project board details for recent and upcoming deliveries
 def ColumnDetails(column_json, column_name, upcoming=False, ending=False):
-    issue_detail = ""
-    for column in column_json:
-        if column['name'] == column_name:
-            for card in column['cards']['nodes']:
-                title = card["content"]["title"]
-                issue_num = card["content"]["number"]
-                # TODO: Pass in REPOSITORY
-                this_issue = '  - [' + title + ']('+ FormatUrl(REPOSITORY, issue_num) + ')\n'
-                date_range = DatesFromIssueTitle(title)
-                if upcoming and date_range['start'] > datetime.date.today() and date_range['start'] < (datetime.date.today()+datetime.timedelta(days=7)):
-                    issue_detail += this_issue
-                elif ending and date_range['finish'] > datetime.date.today() and date_range['finish'] < datetime.date.today()+datetime.timedelta(days=7):
-                    issue_detail += this_issue
-                elif (not upcoming) and (not ending):
-                    issue_detail += this_issue
-    return issue_detail
+  issue_detail = ""
+  for column in column_json:
+    if column['name'] == column_name:
+      for card in column['cards']['nodes']:
+        title = card["content"]["title"]
+        issue_num = card["content"]["number"]
+        # TODO: Pass in ORG & REPOSITORY
+        this_issue = '  - [' + title + ']('+ FormatUrl(ORGANIZATION, REPOSITORY, issue_num) + ')\n'
+        date_range = DatesFromIssueTitle(title)
+        if upcoming and date_range['start'] > datetime.date.today() and date_range['start'] < (datetime.date.today()+datetime.timedelta(days=7)):
+          issue_detail += this_issue
+        elif ending and date_range['finish'] > datetime.date.today() and date_range['finish'] < datetime.date.today()+datetime.timedelta(days=7):
+          issue_detail += this_issue
+        elif (not upcoming) and (not ending):
+          issue_detail += this_issue
+  return issue_detail
 
+#
+# CountChecklist - Parse the issue description to count "[ ]" and "[x]" per 
+def CountChecklist(issue_description):
+  # Pre-engagement Regex - "### Pre((.|\n)*)### Del"
+  preengagement = CountChecklistForRegion("### Pre((.|\n)*)### Del", issue_description)
+
+  # Delivery Regex - "### Pel((.|\n)*)### Post"
+  delivery = CountChecklistForRegion("### Del((.|\n)*)### Post", issue_description)
+  
+  # Post-engagement Regex - "### Post((.|\n)*)$"
+  postengagement = CountChecklistForRegion("### Post((.|\n)*)$", issue_description)
+
+  results = {'pre':preengagement, 'delivery': delivery, 'post': postengagement}
+  return results
+
+def CountChecklistForRegion(regex, issue_description):
+  checked = 0
+  unchecked = 0
+  try:
+    checklist_block = re.search(regex, issue_description).group()
+    unchecked = re.findall(r'- \[ \]', checklist_block)
+    checked = re.findall(r'- \[x\]', checklist_block, re.IGNORECASE)
+  except:
+    print("*** Missing checklist block for regex ", regex)
+
+  results = { 'checked': checked.count('- [x]') + checked.count('- [X]'), 'unchecked': unchecked.count('- [ ]')}
+  return results
 
 #endregion
 
+
+#
+# Begin Main
+#
+
 label_count = {
-    "AMER": { "Triage": 0, "Confirmed": 0 },
-    "EMEA": { "Triage": 0, "Confirmed": 0 },
-    "APAC": { "Triage": 0, "Confirmed": 0 }}
+  "AMER": { "Triage": 0, "Confirmed": 0 },
+  "EMEA": { "Triage": 0, "Confirmed": 0 },
+  "APAC": { "Triage": 0, "Confirmed": 0 }}
 
 #
 # Read issues and summarize issue counts
@@ -171,39 +190,47 @@ label_count = {
 summary_block=""
 issue_data = GetProjectData( ORGANIZATION, PROJECT_NUM, REPOSITORY)
 
-if issue_data:
-    issues = MergeIssueData(issue_data)
+issues = MergeIssueData(issue_data)
 
-    for issue in issues:
-        for region in label_count.keys():
-          if issue["labels"].find(region):            
-            for engage_state in label_count[region].keys():
-                if issue["labels"].find(region) > 0 and issue["labels"].find(engage_state) > 0:
-                    label_count[region][engage_state] += 1
-      
-    issue_summary = """
+for issue in issues:
+  for region in label_count.keys():
+    if issue["Labels"].find(region):            
+      for engage_state in label_count[region].keys():
+        if issue["Labels"].find(region) > 0 and issue["Labels"].find(engage_state) > 0:
+          label_count[region][engage_state] += 1
+    
+issue_summary = """
 ## Regional Status Summary
 | Region | Triage    | Confirmed | Total |
 | ------ | --------- | --------- | ----- |
 """
 
-    for region in label_count.keys():
-        summary = "| " + region + "   | "
-        total = 0
-        for engage_state in label_count[region].keys():
-            summary += str(label_count[region][engage_state]) + "        | "
-            total += label_count[region][engage_state]
-        issue_summary += summary + str(total) + "   |\n"
-else:
-    issue_summary = "ERROR COUNTING REGION LABELS!"
+for region in label_count.keys():
+  summary = "| " + region + "   | "
+  total = 0
+  for engage_state in label_count[region].keys():
+    summary += str(label_count[region][engage_state]) + "        | "
+    total += label_count[region][engage_state]
+  issue_summary += summary + str(total) + "   |\n"
+
+#
+# Checklist stuff for now...
+for issue in issues:
+  results = CountChecklist(issue['Body'])
+  print(issue['Title'], results)
+
+
+
+
+#---------------------------------------------------------------------------------------------
 
 #
 # Project board summary for Confirmed/Scheduled/In Progress/Done
 # Counts for each state
 #
-status_summary = """
-## Engagement Status Summary
-"""
+# status_summary = """
+# ## Engagement Status Summary
+# """
 
 # for node in issues_json['data']['organization']['projectNext']['columns']['nodes']:
 #     status_summary += "- " + node['name'] + ": " + str(len(node['cards']['nodes'])) + "\n"
@@ -232,6 +259,6 @@ status_summary = """
 
 print(issue_summary)
 
-print(status_summary)
+# print(status_summary)
 
 # print(recent_updates)
